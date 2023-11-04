efa33664ba7bff3078dd60ef7f4671c9bdd0d238
ROBIN <robinalexander20@gmail.com>
2023-11-04 00:47:36 -0400

    initial commit

---
+++ contact.js
@@ -0,0 +1,8 @@
+const contact = 13432142;
+const name = "John Doe";
+const email = "john.doe@example.com";
+const marks = () => {
+  return `${Math.floor(Math.random() * 100)}%`;
+};
+
+module.exports = { contact, name, email, marks };
